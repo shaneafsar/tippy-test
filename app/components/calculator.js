ebe84142ccd446e8b4562a0a27de74bbec90c9a0
Shane Afsar <safsar@linkedin.com>
2019-09-15 17:32:55 -0400

    Calculator component with tip and total amount.

---
+++ app/components/calculator.js
@@ -0,0 +1,23 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  totalBill: 0,
+  tipValue: 0,
+  tipAmount: computed('totalBill', 'tipValue', function() {
+    return this.get('totalBill') * parseFloat(this.get('tipValue'));
+  }),
+  totalBillWithTip: computed('totalBill', 'tipAmount', function() {
+    return this.get('tipAmount') + parseFloat(this.get('totalBill'));
+  }),
+  showTotal: computed('totalBillWithTip', function() {
+    return (
+      this.get('totalBillWithTip') > 0 && !isNaN(this.get('totalBillWithTip'))
+    );
+  }),
+  actions: {
+    onTipChange: function(newTipValue) {
+      this.set('tipValue', newTipValue);
+    }
+  }
+});
